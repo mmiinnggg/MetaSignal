07091842b16538ca78c5a2ec8354d17bab6c442e
ming <37299829+muyang9@users.noreply.github.com>
2023-09-24 19:14:46 +0800

    Create travel_time.py

---
+++ metric/travel_time.py
@@ -0,0 +1,21 @@
+from . import BaseMetric
+import numpy as np
+
+class TravelTimeMetric(BaseMetric):
+    def __init__(self, world):
+        self.world = world
+        self.world.subscribe(["avg_travel_time"])
+        self.name = "Average Travel Time"
+        self.reset()
+
+    def update(self, done=False):
+        self.travel_time = self.world.get_info("avg_travel_time")
+
+        return self.eval()
+
+    def eval(self):
+        return self.travel_time
+
+    def reset(self):
+        #self.vehicle_enter_time = {}
+        self.travel_time = []
